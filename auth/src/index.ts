61118b455cac1063dff392831eb9406d532c8a49
Abhinav Dixit <abhinavdixit@192.168.1.4>
2024-03-17 03:52:31 +0530

    setting up ingress nginx

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,13 @@ const app = express();
 
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res)=> {
+    res.send('Testing there!');
+})
+
+
+
 app.listen(3000,()=>{
-console.log("Auth listening on Port 3000"); 
+console.log("Auth listening on Port 3000!!!"); 
     
 });
